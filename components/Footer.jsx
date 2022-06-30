a652d6a05d28e4e099c162405464629c8435d568
unknown <ivanbelasich@gmail.com>
2022-06-30 19:50:46 -0300

    footercss

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,39 +7,41 @@ import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-      <ul className={styles.contact}>
-        <h3>Contacto</h3>
-        <li>
-          <FaPhoneAlt size={"2em"} /> <h4>+543417789563</h4>
-        </li>
-        <li>
-          <AiOutlineMail size={"2em"} /> <h4>cervezaloker@gmail.com</h4>
-        </li>
-      </ul>
-      <h3>Nuestras redes</h3>
-      <ul className={styles.social_links}>
-        <li>
-          <a
-            href="https://www.facebook.com/cervezaloker"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaFacebookSquare size="2.3em" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/cervezaloker/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BsInstagram size="2.3em" />
-          </a>
-        </li>
-      </ul>
-      2022 Cerveza Loker©
-    </div>
+    <>
+      <div className={styles.container}>
+        <ul className={styles.contact}>
+          <h3>Contacto</h3>
+          <li>
+            <FaPhoneAlt size={"2em"} /> <h4>+543417789563</h4>
+          </li>
+          <li>
+            <AiOutlineMail size={"2em"} /> <h4>cervezaloker@gmail.com</h4>
+          </li>
+        </ul>
+        <h3>Nuestras redes</h3>
+        <ul className={styles.social_links}>
+          <li>
+            <a
+              href="https://www.facebook.com/cervezaloker"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookSquare size="2.3em" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/cervezaloker/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsInstagram size="2.3em" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.derechos}>2022 Cerveza Loker©</div>
+    </>
   );
 };
 
