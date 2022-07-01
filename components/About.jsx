a7ea4f8cc77b8471a4dc830fd517c83a1fdc8ef4
unknown <ivanbelasich@gmail.com>
2022-06-30 22:05:45 -0300

    general fixes css

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/About.module.css";
 export const About = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.container_text}>Pasión por la cerveza</div>
+     {/*  <div className={styles.container_text}>Pasión por la cerveza</div> */}
     </div>
   );
 };
